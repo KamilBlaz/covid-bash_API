c79a83c3ebe13c54f6e3da1daf9876a424dbda37
Kamil Błaż <48087257+KamilBlaz@users.noreply.github.com>
2022-03-22 21:41:38 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/kamil/.bashrc
+source ~/.bashrc
 curl --location --request GET -s 'https://thevirustracker.com/free-api?global=stats'  |  jq '.results[0].total_cases' > nos.txt  
-source /home/kamil/.bashrc
+source ~/.bashrc
 
